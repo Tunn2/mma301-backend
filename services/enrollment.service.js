e57d63c357d71113332a5e25317daee3f84fe309
tungnk <tungnk1404@gmail.com>
2025-01-24 16:49:39 +0700

    update ipn

--- services/enrollment.service.js
+++ services/enrollment.service.js
@@ -42,6 +42,7 @@ const getEnrollmentByIdService = async (id) => {
 const updateEnrollmentByIdService = async ({ id, status = "COMPLETED" }) => {
   if (!new mongoose.Types.ObjectId(id))
     throw new Error("Invalid enrollment id");
+  console.log("vcl");
   return await Enrollment.updateOne({ _id: id }, { status });
 };
 
